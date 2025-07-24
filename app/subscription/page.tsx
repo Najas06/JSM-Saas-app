03ca3a636114010d59cb11905f6c475b3af919ee
Najas06 <najasneju@gmail.com>
2025-07-24 22:36:26 +0530

    last sectiom implemented Clerk price table and supabse intregration then created table and policies ( I Stucked in Progesql)

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,8 +1,11 @@
+import { PricingTable } from "@clerk/nextjs"
 
 
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <div className="my-16 md:my-20">
+      <PricingTable/>
+    </div>
   )
 }
 
