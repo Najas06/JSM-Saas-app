4590714ef867155bb3209754d64f6b1ccabdd7e9
Najas06 <najasneju@gmail.com>
2025-07-30 10:03:44 +0530

    implemented Companions Library with search and filter functionality

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+import { getAllCompanions } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
-const CompanionsLibrary = () => {
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filter = await searchParams;
+  // console.log(params);
+  const subject = filter.subject ? filter.subject : "";
+  const topic = filter.topic ? filter.topic : "";
+  const companions = await getAllCompanions({ subject, topic });
+  // console.log(companions);
+
   return (
-    <div>CompanionsLibrary</div>
-  )
-}
+    <main className="my-16 md:my-20">
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companions Library</h1>
+        <div className="flex gap-4">
+          <SearchInput/>
+          <SubjectFilter/>
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
+};
 
-export default CompanionsLibrary
+export default CompanionsLibrary;
